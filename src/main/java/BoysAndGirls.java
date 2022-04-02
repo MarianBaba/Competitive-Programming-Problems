2c1e1dbb5f35b7f46146db53e71eb2e509691a1e
MarianBaba <marianbaba16@gmail.com>
2022-04-02 17:10:25 +0200

    BoysAndGirls solution

--- src/main/java/BoysAndGirls.java
+++ src/main/java/BoysAndGirls.java
@@ -1,9 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class BoysAndGirls {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int nkids = Integer.parseInt(input[0]);
+		int time = Integer.parseInt(input[1]);
+		char[] initial = br.readLine().toCharArray();
+		while(time != 0) {
+			ArrayList<Integer> boysPos = new ArrayList<Integer>();
+			for(int i = 0; i < nkids-1; i++) {
+				if(initial[i] == 'B' && initial[i+1] == 'G') {
+					boysPos.add(i);
+				}
+			}
+			for(int i = 0; i < boysPos.size(); i++) {
+				char temp = initial[boysPos.get(i)];
+				initial[boysPos.get(i)] = initial[boysPos.get(i)+1];
+				initial[boysPos.get(i)+1] = temp; 
+			}
+			time--;
+		}
+		for(char c : initial) {
+			System.out.print(c);
+		}
+	}
 }

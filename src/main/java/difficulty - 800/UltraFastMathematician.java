ccc21370f7dee3bbfcb8e2a3a6b6aa160995b456
MarianBaba <marianbaba16@gmail.com>
2022-04-05 09:52:03 +0200

    UltraFastMathematician

---
+++ src/main/java/difficulty - 800/UltraFastMathematician.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class UltraFastMathematician {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] first = br.readLine().toCharArray();
+        char[] sec = br.readLine().toCharArray();
+        int[] f = new int[first.length];
+        int[] s = new int[sec.length];
+        int[] res = new int[first.length];
+        for (int i = 0; i < f.length; i++) {
+            f[i] = (int) first[i];
+            s[i] = (int) sec[i];
+            res[i] = f[i] ^ s[i];
+            System.out.print(res[i]);
+        }
+    }
+}

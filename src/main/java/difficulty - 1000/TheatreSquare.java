acc389df40adf0352f24b85ae7e203a2ba248293
MarianBaba <marianbaba16@gmail.com>
2022-04-14 10:01:37 +0200

    TheatreSquare

---
+++ src/main/java/difficulty - 1000/TheatreSquare.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TheatreSquare {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        Long n = Long.parseLong(inputs[0]);
+        Long m = Long.parseLong(inputs[1]);
+        Long a = Long.parseLong(inputs[2]);
+
+        if (a == 1) {
+            System.out.println(n * m);
+        } else {
+            Long c1 = 0L;
+            while (n > 0) {
+                n -= a;
+                c1++;
+            }
+            Long c2 = 0L;
+            while (m > 0) {
+                m -= a;
+                c2++;
+            }
+            Long res = c1 * c2;
+            System.out.println(res);
+        }
+    }
+}
